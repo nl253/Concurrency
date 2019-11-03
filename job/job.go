5d9ca29200da8cafbdcea6fe2a6cceaa14d1a27e
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-11-03 19:39:05 +0000

    Add group functions

--- job/job.go
+++ job/job.go
@@ -12,6 +12,46 @@ type AsyncJob struct {
 	val     interface{}
 }
 
+func All(js ...*AsyncJob) *AsyncJob {
+	return NewClojure(func() interface{} {
+		n := uint(len(js))
+		xs := make([]interface{}, n)
+		for i := uint(0); i < n; i++ {
+			xs[i] = js[i].Await()
+		}
+		return xs
+	})
+}
+
+func Await(js ...*AsyncJob) {
+	NewClojure(func() interface{} {
+		n := uint(len(js))
+		for i := uint(0); i < n; i++ {
+			js[i].Await()
+		}
+		return nil
+	}).Await()
+}
+
+func Race(js ...*AsyncJob) *AsyncJob {
+	return NewClojure(func() interface{} {
+		var result interface{} = nil
+		done := false
+		n := uint(len(js))
+		ws := make([]*AsyncJob, len(js), len(js))
+		for idx := uint(0); idx < n; idx++ {
+			ws[idx] = NewConsumer(func(args ...interface{}) {
+				if !done {
+					result = js[args[0].(uint)].Start().Await()
+					done = true
+				}
+			}).Start(idx)
+		}
+		Await(ws...)
+		return result
+	})
+}
+
 func New(f func(...interface{}) interface{}) *AsyncJob {
 	return &AsyncJob{
 		running: false,
