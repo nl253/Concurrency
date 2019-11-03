b40ee6e78c794306aea95a7cc9de31a1aed87349
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-11-03 20:16:39 +0000

    Add Running

--- job/job.go
+++ job/job.go
@@ -1,64 +1,81 @@
 package job
 
 import (
-	"fmt"
-	"sync"
+    "fmt"
+    "sync"
 )
 
 type AsyncJob struct {
-	running bool
-	lk      *sync.Mutex
-	f       func(...interface{}) interface{}
-	val     interface{}
+	f func(...interface{}) interface{}
 }
 
-func All(js ...*AsyncJob) *AsyncJob {
+type Running struct {
+	lk  *sync.Mutex
+	val interface{}
+}
+
+func Await(js ...*AsyncJob) *AsyncJob {
 	return NewClojure(func() interface{} {
-		n := uint(len(js))
-		xs := make([]interface{}, n)
-		for i := uint(0); i < n; i++ {
-			xs[i] = js[i].Await()
-		}
-		return xs
+		return AwaitRunning(Dispatch(js...)...)
 	})
 }
 
-func Await(js ...*AsyncJob) {
-	NewClojure(func() interface{} {
-		n := uint(len(js))
-		for i := uint(0); i < n; i++ {
-			js[i].Await()
-		}
-		return nil
-	}).Await()
+func AwaitRunning(js ...*Running) []interface{} {
+	n := uint(len(js))
+	xs := make([]interface{}, n)
+	for i := uint(0); i < n; i++ {
+		xs[i] = js[i].Await()
+	}
+	return xs
+}
+
+func Consume(js ...*AsyncJob) {
+	ConsumeRunning(Dispatch(js...)...)
+}
+
+func ConsumeRunning(js ...*Running) {
+	n := uint(len(js))
+	for i := uint(0); i < n; i++ {
+		js[i].Await()
+	}
+}
+
+func Dispatch(js ...*AsyncJob) []*Running {
+	n := uint(len(js))
+	ws := make([]*Running, n)
+	for i := uint(0); i < n; i++ {
+		ws[i] = js[i].Start()
+	}
+	return ws
 }
 
 func Race(js ...*AsyncJob) *AsyncJob {
 	return NewClojure(func() interface{} {
+		return RaceRunning(Dispatch(js...)...)
+	})
+}
+
+func RaceRunning(js ...*Running) *Running {
+	return NewClojure(func() interface{} {
 		var result interface{} = nil
 		done := false
 		n := uint(len(js))
-		ws := make([]*AsyncJob, len(js), len(js))
+		ws := make([]*Running, n, n)
 		for idx := uint(0); idx < n; idx++ {
 			ws[idx] = NewConsumer(func(args ...interface{}) {
 				if !done {
-					result = js[args[0].(uint)].Start().Await()
+					result = js[args[0].(uint)].Await()
 					done = true
 				}
 			}).Start(idx)
 		}
-		Await(ws...)
+		ConsumeRunning(ws...)
 		return result
-	})
+	}).Start()
 }
 
 func New(f func(...interface{}) interface{}) *AsyncJob {
-	return &AsyncJob{
-		running: false,
-		lk:      &sync.Mutex{},
-		f:       f,
-		val:     nil,
-	}
+	return &AsyncJob{f}
 }
 
 func NewConsumer(f func(...interface{})) *AsyncJob {
@@ -74,28 +91,31 @@ func NewClojure(f func() interface{}) *AsyncJob {
 	})
 }
 
-func (j *AsyncJob) Start(args ...interface{}) *AsyncJob {
-	j.lk.Lock()
-	j.running = true
+func (j *AsyncJob) Start(args ...interface{}) *Running {
+	p := &Running{
+		lk:  &sync.Mutex{},
+		val: nil,
+	}
+	p.lk.Lock()
 	go func() {
-		j.val = j.f(args...)
-		j.running = false
-		j.lk.Unlock()
+		p.val = j.f(args...)
+		p.lk.Unlock()
 	}()
-	return j
+	return p
 }
 
-func (j *AsyncJob) Await() interface{} {
+func (j *Running) Await() interface{} {
 	j.lk.Lock()
 	defer j.lk.Unlock()
 	return j.val
 }
 
-func (j *AsyncJob) String() string {
+func (j *Running) String() string {
 	j.lk.Lock()
 	defer j.lk.Unlock()
-	if j.running {
-		return "AsyncJob { running }"
-	}
 	return fmt.Sprintf("AsyncJob { result = %v (%T) }", j.val, j.val)
+}
+
+func (j *AsyncJob) String() string {
+	return "AsyncJob"
 }
