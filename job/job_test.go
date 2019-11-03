43d03293afc634926b79a7805092b799c28319f1
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-11-03 18:59:46 +0000

    More meaningful test

--- job/job_test.go
+++ job/job_test.go
@@ -1,6 +1,7 @@
 package job
 
 import (
+	"math/rand"
 	"testing"
 
 	ut "github.com/nl253/Testing"
@@ -8,17 +9,18 @@ import (
 
 var fJob = ut.Test("Worker")
 
+const N uint = 10000
+
 func TestActor_Start(t *testing.T) {
 	should := fJob("Start", t)
-}
-
-func TestActor_Done(t *testing.T) {
-	should := fJob("Running", t)
-}
-
-func TestActor_String(t *testing.T) {
-	should := fJob("String", t)
-	should("stringify converts to string", true, func() interface{} {
-		return ""
-	})
+	for i := uint(0); i < N; i++ {
+		expect := rand.Int()
+		should("not crash", expect, func() interface{} {
+			return NewClojure(func() interface{} { return expect }).Start().Await()
+		})
+		expect2 := rand.Int()
+		should("not crash when run with args", expect2+expect, func() interface{} {
+			return New(func(xs ...interface{}) interface{} { return xs[0].(int) + xs[1].(int) }).Start(expect, expect2).Await()
+		})
+	}
 }
