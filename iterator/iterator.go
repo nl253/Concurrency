02b61632768c0415660e28dc0680aa69137334ce
Norbert Logiewa <norbertlogiewa96@gmail.com>
2019-11-09 11:08:16 +0000

    Add logging

--- iterator/iterator.go
+++ iterator/iterator.go
@@ -1,7 +1,11 @@
 package iterator
 
 import (
+	"fmt"
+	"math/rand"
 	"sync"
+
+	"github.com/nl253/DataStructures/list"
 )
 
 type Iterator struct {
@@ -16,8 +20,14 @@ var EndOfIteration = &finished{}
 
 func New(initState interface{}, f func(interface{}) interface{}) *Iterator {
 	return &Iterator{
-		lk:    &sync.Mutex{},
-		f:     f,
+		lk: &sync.Mutex{},
+		f: func(state interface{}) interface{} {
+			if state == EndOfIteration {
+				return state
+			} else {
+				return f(state)
+			}
+		},
 		state: initState,
 	}
 }
@@ -30,6 +40,14 @@ func Ints() *Iterator {
 	return Range(0, 1)
 }
 
+func Floats() *Iterator {
+	return New(rand.Float64(), func(_ interface{}) interface{} { return rand.Float64() })
+}
+
+func Bytes() *Iterator {
+	return New(rand.Uint32(), func(_ interface{}) interface{} { return byte(rand.Uint32()) })
+}
+
 func (iter *Iterator) Sum() int {
 	return iter.ReduceAll(func(x interface{}, y interface{}) interface{} { return x.(int) + y.(int) }).(int)
 }
@@ -39,7 +57,23 @@ func (iter *Iterator) Count() int {
 }
 
 func Repeat(x interface{}) *Iterator {
-	return New(x, func(_ interface{}) interface{} { return x })
+	return New(x, func(y interface{}) interface{} { return y })
+}
+
+func (iter *Iterator) Slice(n uint) *Iterator {
+	iter.lk.Lock()
+	defer iter.lk.Unlock()
+	if n == 0 {
+		return Repeat(EndOfIteration)
+	}
+	return New(iter.pull(), func(state interface{}) interface{} {
+		if n <= 1 {
+			return EndOfIteration
+		} else {
+			n--
+			return iter.pull()
+		}
+	})
 }
 
 func (iter *Iterator) Peek() interface{} {
@@ -54,51 +88,83 @@ func (iter *Iterator) Pull() interface{} {
 	return iter.pull()
 }
 
+func (iter *Iterator) tryAdvance() bool {
+	ok := iter.state != EndOfIteration
+	if ok {
+		iter.state = iter.f(iter.state)
+	}
+	return ok
+}
+
 func (iter *Iterator) pull() interface{} {
-	result := iter.state
-	iter.state = iter.f(iter.state)
-	return result
+	defer iter.tryAdvance()
+	return iter.state
 }
 
 func (iter *Iterator) PullN(n uint) []interface{} {
 	iter.lk.Lock()
 	defer iter.lk.Unlock()
 	result := make([]interface{}, n)
-	result[0] = iter.state
-	for j := uint(1); j < n; j++ {
-		result[j] = iter.f(result[j-1])
+	for i := uint(0); i < n; i++ {
+		result[i] = iter.pull()
 	}
-	iter.state = result[n-1]
 	return result
 }
 
-func (iter *Iterator) PullAll() []interface{} {
+func (iter *Iterator) PullAll() *list.ConcurrentList {
 	iter.lk.Lock()
 	defer iter.lk.Unlock()
-	result := make([]interface{}, 1)
-	result[0] = iter.state
-	for focus := iter.pull(); focus != EndOfIteration; focus = iter.pull() {
-		result = append(result, focus)
+	result := list.New()
+	for iter.state != EndOfIteration {
+		result.Append(iter.pull())
 	}
 	return result
 }
 
-func (iter *Iterator) Skip() {
-	iter.SkipN(1)
+func (iter *Iterator) Skip() *Iterator {
+	return iter.SkipN(1)
 }
 
-func (iter *Iterator) SkipN(n uint) {
+func (iter *Iterator) skip() *Iterator {
+	return iter.skipN(1)
+}
+
+func (iter *Iterator) SkipN(n uint) *Iterator {
 	iter.lk.Lock()
-	for j := uint(0); j < n; j++ {
-		iter.state = iter.f(iter.state)
+	defer iter.lk.Unlock()
+	return iter.skipN(n)
+}
+
+func (iter *Iterator) skipN(n uint) *Iterator {
+	for i := uint(0); i < n; i++ {
+		if !iter.tryAdvance() {
+			return iter
+		}
 	}
-	iter.lk.Unlock()
+	return iter
 }
 
 func (iter *Iterator) Map(f func(interface{}) interface{}) *Iterator {
 	iter.lk.Lock()
 	defer iter.lk.Unlock()
-	return New(f(iter.state), func(state interface{}) interface{} { return f(iter.Pull()) })
+	return New(f(iter.pull()), func(state interface{}) interface{} { return f(iter.Pull()) })
+}
+
+func (iter *Iterator) Printf(format string) *Iterator {
+	return iter.Tap(func(x interface{}) { fmt.Printf(format, x) })
+}
+
+func (iter *Iterator) Println() *Iterator {
+	return iter.Printf("%v\n")
+}
+
+func (iter *Iterator) Tap(f func(interface{})) *Iterator {
+	return iter.Map(func(x interface{}) interface{} {
+		if x != EndOfIteration {
+			f(x)
+		}
+		return x
+	})
 }
 
 func (iter *Iterator) Filter(f func(interface{}) bool) *Iterator {
@@ -117,8 +183,7 @@ func (iter *Iterator) ReduceN(n uint, f func(interface{}, interface{}) interface
 	iter.lk.Lock()
 	defer iter.lk.Unlock()
 	acc := iter.pull()
-	n--
-	for i := uint(0); i < n; i++ {
+	for i := uint(1); i < n; i++ {
 		acc = f(acc, iter.pull())
 	}
 	return acc
@@ -128,12 +193,13 @@ func (iter *Iterator) ReduceAll(f func(interface{}, interface{}) interface{}) in
 	iter.lk.Lock()
 	defer iter.lk.Unlock()
 	acc := iter.pull()
-	if acc != EndOfIteration {
-		for peek := iter.pull(); peek != EndOfIteration; {
-			acc = f(acc, peek)
+	for {
+		if x := iter.pull(); x != EndOfIteration {
+			acc = f(acc, x)
+		} else {
+			return acc
 		}
 	}
-	return acc
 }
 
 func (iter *Iterator) Clone() *Iterator {
